8956615c2531919690afde38801151e9e62b04a3
Dmitriy Filatov <filatov-d@mail.ru>
2022-11-25 17:28:04 +0300

    Task 2 completed

--- HW_2/task_2.py
+++ HW_2/task_2.py
@@ -8,3 +8,16 @@
 4
 [1, 2, 6, 24]
 """
+# Ниже закоменчен тоже рабочий код
+# num = int(input('Введите натуральное число: '))
+# count, fact_list = 2, [1]
+# for i in range(num - 1):
+#     fact_list.append(fact_list[i] * count)
+#     count += 1
+# print(fact_list)
+
+num = int(input('Введите натуральное число: '))
+fact_list = [1] * num
+for i in range(1, num):
+    fact_list[i] = fact_list[i - 1] * (i + 1)
+print(fact_list)
