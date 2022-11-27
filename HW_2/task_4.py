0b5cf1d6ef00bf5582c3e45b67de1afcd0821798
Dmitriy Filatov <filatov-d@mail.ru>
2022-11-27 21:35:32 +0300

    Task 4 completed

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -6,3 +6,26 @@
 Ввод: значение типа <int>
 Вывод: значение типа <int>
 """
+
+# file_name = 'HW_2/indexes.txt'
+
+# with open(file_name, 'w', encoding='utf-8') as file:
+
+#     file.writelines()
+
+import random
+
+n = int(input('Укажите длину списка: '))
+my_list = [random.randint(-n, n) for i in range(n)]
+result = 1
+for i in range(n):
+    file_indexes = 'HW_2/indexes.txt'
+    with open(file_indexes) as file_indexes:
+        
+            for line in file_indexes:
+                x = int(line)
+                if i == x:
+                    result = my_list[i] * result
+                    print(my_list[i], result)
+                    print(x)
+print(my_list, result)
