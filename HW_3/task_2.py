653e99b2be56403354be603ef180147ec3ed9ea1
Dmitriy Filatov <filatov-d@mail.ru>
2022-11-29 15:24:28 +0300

    Task 2 completed

--- HW_3/task_2.py
+++ HW_3/task_2.py
@@ -7,9 +7,21 @@
 Вывод: значение типа <list>
 
 Пример:
-[2, 3, 4, 5, 6]
+[ 2, 3, 4, 5, 6]
 [12, 15, 16]
 
-[2, 3, 5, 6]
+[ 2, 3, 5, 6]
 [12, 15]
 """
+
+import random
+size_list = int(input('Введите размер списка: '))
+list_nums = [random.randint(0, 9) for i in range(size_list)]
+result_list = []
+
+for i in range(size_list //  2):
+    result_list.append(list_nums[i] * list_nums[-i - 1])
+if size_list %  2 != 0:
+    result_list.append(list_nums[round(size_list /  2)] ** 2)
+print(f'Исходный список: {list_nums}')    
+print(f'Итоговый список: {result_list}')
