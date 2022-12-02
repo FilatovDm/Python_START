f3b8b4bc91ffd52536cff42d2b62495092d00bb3
Dmitriy Filatov <filatov-d@msil.ru>
2022-12-02 11:39:10 +0300

    Task 2 completed

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,14 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+
+from random import randrange
+
+size_list = int(input('Введите размер списка: '))
+rand_list = [randrange(9) for i in range(size_list)]
+result_list = []
+print(rand_list)
+for i in range(size_list):
+    if rand_list.count(rand_list[i]) == 1:
+        result_list.append(rand_list[i])
+print(result_list)
