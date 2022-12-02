a29810319020ade42fed779e676598bae9143a51
Dmitriy Filatov <filatov-d@msil.ru>
2022-12-02 10:52:51 +0300

    Task 1 completed

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -1,6 +1,5 @@
 """
 Выведите список простых множителей натурального числа N.
-
 Ввод: значение типа <int>
 Вывод: значение типа <list>
 
@@ -11,3 +10,16 @@
 38
 [2, 19]
 """
+
+num = int(input('Введите натуральное число: '))
+result_list = []
+digit = 2
+while digit ** 2 <= num:
+    if num % digit == 0:
+        result_list.append(digit)
+        num //= digit
+    else:
+        digit += 1
+if num > 1:
+    result_list.append(num)
+print(result_list)
