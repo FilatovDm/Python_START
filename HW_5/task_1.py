4c6927326c7fe53cae84d2f8534e1d0dc4a5844d
Dmitriy Filatov <filatov-d@mail.ru>
2022-12-12 13:11:31 +0300

    Task 1 completed

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,9 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+
+stroke = 'азбука автобус автомобиль аптека библиотека'
+words = 'абв'
+
+result = ' '.join(filter(lambda x: not (words[0] in x and words[1] in x and words[2] in x), stroke.split()))
+print(result)
