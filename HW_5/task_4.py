adeed9cf2ca35195d6a05e6d7110a3bd8d143b5c
Dmitriy Filatov <filatov-d@mail.ru>
2022-12-12 16:22:26 +0300

    Task 4 completed

--- HW_5/task_4.py
+++ HW_5/task_4.py
@@ -10,3 +10,29 @@
 ыыыыыррррр   аааааагггггггг
 5ы5р3 6а8г
 """
+
+input_line = input()
+count = 1
+new_line = ''
+
+for i in range(1, len(input_line)):
+    if input_line[i] == input_line[i - 1]:
+        if count == 9:
+            new_line += str(count) + input_line[i-1]
+            count = 1
+        count += 1
+    else: 
+        new_line += str(count) + input_line[i-1]
+        count = 1
+        continue
+
+new_line += str(count)  + input_line[-1]      
+print(new_line)
+
+def decoding(code):
+    decode = ''
+    for i in range(0, len(code), 2):
+        count2, symbol = code[i: i + 2]
+        decode += symbol * int(count2)
+    return decode
+print(decoding(new_line))
