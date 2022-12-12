3788483cb2579b33801b9ca265dae85c663b0ba6
Dmitriy Filatov <filatov-d@mail.ru>
2022-12-12 16:46:45 +0300

    Task 2 completed

--- HW_5/task_2.py
+++ HW_5/task_2.py
@@ -9,3 +9,47 @@
 a) Добавьте игру против бота
 b) Подумайте, как наделить бота простейшим "интеллектом"
 """
+
+from random import randint
+
+def input_dat(name, max_candy):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до {max_candy}: "))
+    while x < 1 or x > max_candy:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, took, counter, value):
+    print(f"Ходил {name}, он взял {took}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+player1 = input("Введите имя первого игрока: ")
+player2 = input("Введите имя второго игрока: ")
+N = int(input("Введите количество конфет на столе: "))
+max_candy = int(input("Введите максимальное количество конфет, которое можно взять за одни ход: "))
+flag = randint(0,2)
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0 
+counter2 = 0
+
+while N > max_candy:
+    if flag:
+        took = input_dat(player1, max_candy)
+        counter1 += took
+        N -= took
+        flag = False
+        p_print(player1, took, counter1, N)
+    else:
+        took = input_dat(player2, max_candy)
+        counter2 += took
+        N -= took
+        flag = True
+        p_print(player2, took, counter2, N)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
